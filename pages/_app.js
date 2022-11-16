c35321de80b22e56d941283dee9795c932999861
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2022-11-16 12:12:03 -0500

    Proyecto

---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { LayoutProvider } from '../layout/context/layoutcontext';
+import Layout from '../layout/layout';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import '../styles/layout/layout.scss';
+import '../styles/demo/Demos.scss';
+import Landing from "./landing";
+
+export default function MyApp({ Component, pageProps }) {
+    if (Component.getLayout) {
+        return (
+            <LayoutProvider>
+                {Component.getLayout(<Component {...pageProps} />)}
+            </LayoutProvider>
+        )
+    } else {
+        return (
+            <LayoutProvider>
+                {/*<Layout>*/}
+                {/*    <Component {...pageProps} />*/}
+                {/*</Layout>*/}
+
+                <Landing>
+
+                </Landing>
+            </LayoutProvider>
+        );
+    }
+}
